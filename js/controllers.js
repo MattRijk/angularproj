1d0460788d9f2d2aa05dbfc9953f4ec28cd53a0a
Matt Rijk <matthewrijk@gmail.com>
2015-04-14 10:21:29 -0400

    start of angular project

---
+++ js/controllers.js
@@ -0,0 +1,10 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyController', function MyController($scope){
+
+	$scope.author = {
+		'name': 'Matt Rijk',
+		'title': 'Staff',
+		'company': 'Natick Web'
+	}
+ });
